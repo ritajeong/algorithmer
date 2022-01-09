2d07a809a64248acb5ab0fb098cb1728b6c71a68
Jinju <48556400+ritajeong@users.noreply.github.com>
2022-01-09 22:28:41 +0900

    Create pro단어변환.java

---
+++ pro단어변환.java
@@ -0,0 +1,9 @@
+/*
+1시간 정도 고민했는데 도저히 떠오르지 않아 솔루션을 참고했습니다.  
+dfs 그림보고 이해가 되었습니다.  
+https://velog.io/@euneun/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-%EB%8B%A8%EC%96%B4-%EB%B3%80%ED%99%98BFSDFS-C-v5lnyekn  
+
+최소거리를 구하는 문제라 bfs가 더 좋은 해답이라는 글을 봤는데, bfs 풀이가 아직 이해가 안 가네요  
+https://velog.io/@eunseokim/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-DFSBFS-%EB%8B%A8%EC%96%B4-%EB%B3%80%ED%99%98  
+위 링크에서 그림자료가 도움이 되었지만 아직 완전히 이해하진 못했어요  
+*/
