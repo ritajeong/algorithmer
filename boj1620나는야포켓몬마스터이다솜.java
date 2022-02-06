5a63e5bae8ec33067f2749cfb23e0a27305007fc
Jinju <48556400+ritajeong@users.noreply.github.com>
2022-02-06 22:53:25 +0900

    Update boj1620나는야포켓몬마스터이다솜.java

--- boj1620나는야포켓몬마스터이다솜.java
+++ boj1620나는야포켓몬마스터이다솜.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.*;
-
+//53588KB	476ms
 class Main {  
   public static void main(String[] args) throws Exception {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
