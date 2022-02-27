52280b4c36f6753ee71e8263ee97a6800efe6216
Jinju <48556400+ritajeong@users.noreply.github.com>
2022-02-27 23:14:16 +0900

    Update pro기능개발.java

--- pro기능개발.java
+++ pro기능개발.java
@@ -1,23 +1,35 @@
+/*
+테스트 1 〉	통과 (0.20ms, 78.5MB)
+테스트 2 〉	통과 (0.36ms, 82.6MB)
+테스트 3 〉	통과 (0.24ms, 76.3MB)
+테스트 4 〉	통과 (0.25ms, 72.4MB)
+테스트 5 〉	통과 (0.20ms, 76MB)
+테스트 6 〉	통과 (0.20ms, 79.7MB)
+테스트 7 〉	통과 (0.27ms, 78.7MB)
+테스트 8 〉	통과 (0.20ms, 74.5MB)
+테스트 9 〉	통과 (0.22ms, 77.5MB)
+테스트 10 〉	통과 (0.22ms, 81.8MB)
+테스트 11 〉	통과 (0.22ms, 79.8MB)
+*/
+import java.util.Arrays;
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        int[] answer = new int[progresses.length];
-        int days[] = new int[progresses.length];
-        
+        int[] days = new int[progresses.length];
+        int[] answer = new int[progresses.length+1];
+        int maxday = days[0];
+        int endOfIndex = 0;
+
         for (int i = 0; i < progresses.length; i++) {
             days[i] = ((int) Math.ceil( (double) (100 - progresses[i]) / speeds[i]));
-        }
 
-        int maxday = days[0];
-
-        for (int i = 0, j = 0; i < progresses.length; i++) {
             if (days[i] <= maxday) {
-                answer[j] += 1;
+                answer[endOfIndex] += 1;
             } else {
                 maxday = days[i];
-                answer[++j] = 1;
+                answer[++endOfIndex] = 1;
             }
         }
         
-        return answer;
+        return Arrays.copyOfRange(answer, 1,endOfIndex+1);
     }
 }
